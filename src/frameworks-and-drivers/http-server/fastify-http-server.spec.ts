3db41da205ac9c7a7b0fb920ad4220ca487688f2
Douglas Silva <douglaseusilva@gmail.com>
2024-06-10 16:56:11 -0300

    test: garantir execução do preController antes do controller, repassando os dados

--- src/frameworks-and-drivers/http-server/fastify-http-server.spec.ts
+++ src/frameworks-and-drivers/http-server/fastify-http-server.spec.ts
@@ -32,7 +32,11 @@ describe('Http Server with Express', () => {
   describe('route registration through an controller', () => {
     
     test('return controller response if is listening and route exists', async () => {
-      sut.registerController(HTTP_VERBS.GET, '/any_route', controller)
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route',
+        controller
+      })
   
       server = await sut.start(9999)
       const response = await fetch(`http://localhost:9999/any_route`)
@@ -41,7 +45,11 @@ describe('Http Server with Express', () => {
     })
   
     test('can forward query params to controller', async () => {
-      sut.registerController(HTTP_VERBS.GET, '/any_route', controller)
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route',
+        controller
+      })
   
       server = await sut.start(9999)
       await fetch(`http://localhost:9999/any_route?name=any_name`)
@@ -56,7 +64,11 @@ describe('Http Server with Express', () => {
     })
   
     test('can forward named params to controller', async () => {
-      sut.registerController(HTTP_VERBS.GET, '/any_route/:name', controller)
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route/:name',
+        controller
+      })
   
       server = await sut.start(9999)
       await fetch(`http://localhost:9999/any_route/any_name`)
@@ -71,7 +83,11 @@ describe('Http Server with Express', () => {
     })
   
     test('can forward a body to controller through POST request', async () => {
-      sut.registerController(HTTP_VERBS.POST, '/any_route', controller)
+      sut.registerControllerV2({
+        method: HTTP_VERBS.POST,
+        route: '/any_route',
+        controller
+      })
   
       server = await sut.start(9999)
       await fetch(`http://localhost:9999/any_route`, {
@@ -89,6 +105,82 @@ describe('Http Server with Express', () => {
               { name: 'any_name' }
           )
         )
+    })
+
+    test('can run a preController before the controller', async () => {
+      const preController = mock<HttpController>()
+      preController.handle.mockResolvedValue({ statusCode: 200, body: '' })
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route',
+        controller,
+        preController
+      })
+  
+      server = await sut.start(9999)
+      await fetch(`http://localhost:9999/any_route`, {
+        method: HTTP_VERBS.GET,
+      })
+
+      expect(preController.handle).toHaveBeenCalledOnce()
+      expect(controller.handle).toHaveBeenCalledOnce()
+    })
+
+    test('controller can get body from preController', async () => {
+      const preController = mock<HttpController>()
+      preController.handle.mockResolvedValue({ statusCode: 200, body: { name: 'any_name'} })
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route',
+        controller,
+        preController
+      })
+  
+      server = await sut.start(9999)
+      await fetch(`http://localhost:9999/any_route`, {
+        method: HTTP_VERBS.GET,
+      })
+
+      expect(controller.handle).toHaveBeenCalledWith(
+        expect.objectContaining({ name: 'any_name' })
+      )
+    })
+
+    test('return http response from preController if statusCode is not 200', async () => {
+      const preController = mock<HttpController>()
+      preController.handle.mockResolvedValue({ statusCode: 400, body: '' })
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route',
+        controller,
+        preController
+      })
+  
+      server = await sut.start(9999)
+      const response = await fetch(`http://localhost:9999/any_route`, {
+        method: HTTP_VERBS.GET,
+      })
+
+      expect(response.status).toBe(400)
+    })
+
+    test('controller not handled if statusCode of preController is not 200', async () => {
+      const preController = mock<HttpController>()
+      preController.handle.mockResolvedValue({ statusCode: 400, body: '' })
+      sut.registerControllerV2({
+        method: HTTP_VERBS.GET,
+        route: '/any_route',
+        controller,
+        preController
+      })
+  
+      server = await sut.start(9999)
+      const response = await fetch(`http://localhost:9999/any_route`, {
+        method: HTTP_VERBS.GET,
+      })
+
+      expect(preController.handle).toHaveBeenCalledOnce()
+      expect(controller.handle).not.toHaveBeenCalled()
     })
   })
 
