aaf142dbb5d8b47c09b8f2bdc758787004712d20
Douglas Silva <douglaseusilva@gmail.com>
2024-08-06 22:56:04 -0300

    fix: retorno de erros com o Boom do Hapi

--- src/frameworks-and-drivers/http-server/hapi-http-server.ts
+++ src/frameworks-and-drivers/http-server/hapi-http-server.ts
@@ -1,32 +1,21 @@
 import { Server } from 'node:http';
 
-import Boom from '@hapi/boom';
 import { Server as HapiServer, server } from '@hapi/hapi';
 
 import { HttpServer, RegisterControllerInput } from "./http-server";
 import { ServerError } from '../../interface-adapters/errors/server-error';
 import { UnauthorizedError } from '../../interface-adapters/errors/unathorized-error';
 import { HTTP_STATUS_CODE } from '../../interface-adapters/http/helpers';
+import { badRequest, unauthorized } from '@hapi/boom';
 
 export class HapiHttpServer implements HttpServer {
   private httpServer: HapiServer
 
   constructor () {
-    this.httpServer = server({
-      debug: {
-        log: 'debug'
-      }
-    })
-    this.httpServer.ext('onPreResponse', (request, response) => {
-      if (request.response && request.response instanceof Boom.Boom && request.response.isBoom) {
-        return response.response(request.response.output.payload).code(request.response.output.statusCode);
-      }
-      return response.continue;
-    })
+    this.httpServer = server()
   }
 
   registerController({ method, route, controller }: RegisterControllerInput): void {
-    const assign = `preController_${method}_${route}`
     this.httpServer.route({
       method: method,
       path: route.replace(/:/g, ""),
@@ -35,8 +24,7 @@ export class HapiHttpServer implements HttpServer {
           ...request.payload as object || {},
           ...request.params || {},
           ...request.query || {},
-          ...request.headers || {},
-          ...request.pre[assign]
+          ...request.headers || {}
         })
 
         if (controllerResponse.error instanceof ServerError) {
@@ -45,25 +33,15 @@ export class HapiHttpServer implements HttpServer {
           .code(HTTP_STATUS_CODE.SERVER_ERROR)
         }
         if (controllerResponse.error instanceof UnauthorizedError) {
-          return response
-          .response(controllerResponse.error)
-          .code(HTTP_STATUS_CODE.UNAUTHORIZED)
+          throw unauthorized(controllerResponse.error)
         }
         if (controllerResponse.error instanceof Error) {
-          return response
-          .response(controllerResponse.error)
-          .code(HTTP_STATUS_CODE.BAD_REQUEST)
+          throw badRequest(controllerResponse.error)
         }
         return response
           .response(controllerResponse.data)
           .code(HTTP_STATUS_CODE.OK)
       }
-    })
-    this.httpServer.ext('onPreResponse', (request, response) => {
-      if (request.response && request.response instanceof Boom.Boom && request.response.isBoom) {
-        return response.response(request.response.output.payload).code(request.response.output.statusCode);
-      }
-      return response.continue;
     })
   }
 
