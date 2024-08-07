2b4755fde5e5d179473fb2f8b5e629c37a4d0208
Douglas Silva <douglaseusilva@gmail.com>
2024-08-07 09:10:01 -0300

    feat: registrar comando com argumentos e encaminhar para o controller

--- src/frameworks-and-drivers/command-line-interface/command-line-interface.ts
+++ src/frameworks-and-drivers/command-line-interface/command-line-interface.ts
@@ -3,9 +3,11 @@ import { Controller } from "../../interface-adapters/controllers/controller"
 export interface RegisterControllerToCommandLineInterfaceInput {
   name: string,
   controller: Controller
+  args?: string
+  options?: string
 }
 
 export interface CommandLineInterface {
   registerController(input: RegisterControllerToCommandLineInterfaceInput): void
-  start(command: string[]): void
+  start(command: string): void
 }
--- src/frameworks-and-drivers/command-line-interface/commander-command-line-interface.ts
+++ src/frameworks-and-drivers/command-line-interface/commander-command-line-interface.ts
@@ -7,19 +7,37 @@ export class CommanderCommandLineInterface implements CommandLineInterface {
     private readonly program: Command = new Command()
   ) {}
 
+  private adaptArgsToControllerInput (args: string[]): object {
+    if (args.length === 0) {
+      return {}
+    }
+    if (args.length % 2 !== 0) {
+      throw new Error('Invalid number of arguments')
+    }
+    const controllerInput: any = {}
+    for (let i = 0; i < args.length; i += 2) {
+      controllerInput[args[i]] = args[i + 1]
+    }
+    return controllerInput
+  }
+
   registerController(input: RegisterControllerToCommandLineInterfaceInput): void {
-    this.program
+    const command = this.program
     .command(input.name)
-    .action(async (/* _, _command: Command */) => {
+    .action(async () => {
       const controller = input.controller
-      const response = await controller.handle({})
+      const controllerInput = this.adaptArgsToControllerInput(command.args || [])
+      const response = await controller.handle(controllerInput)
       if (response.error) {
         this.program.error(response.error?.message)
       }
     })
+    if (input.args) {
+      command.arguments(input.args)
+    }
   }
   
-  start(command: string[]): void {
-    this.program.parse(command, {from: 'user'})
+  start(command: string): void {
+    this.program.parse(command.split(' '), {from: 'user'})
   }
 }
