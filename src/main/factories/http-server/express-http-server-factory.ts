c8aa7b7fbd718d7cb4b3793bde4207609d364d21
Douglas Silva <douglaseusilva@gmail.com>
2024-08-08 23:33:15 -0300

    fix: corrigir import

--- src/main/factories/http-server/express-http-server-factory.ts
+++ src/main/factories/http-server/express-http-server-factory.ts
@@ -1,6 +1,6 @@
 import { ExpressHttpServer } from "../../../frameworks-and-drivers/http-server/express-http-server";
 import { HttpServer } from "../../../frameworks-and-drivers/http-server/http-server";
-import { HttpServerFactory } from "../../http-server-factory";
+import { HttpServerFactory } from "./http-server-factory";
 
 export class ExpressHttpServerFactory implements HttpServerFactory {
   makeHttpServer(): HttpServer {
