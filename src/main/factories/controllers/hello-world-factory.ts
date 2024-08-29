655aea0fa20d4666937c8e33cd4daeb3b875778d
Douglas Silva <douglaseusilva@gmail.com>
2024-08-29 18:13:08 -0300

    feat: factory de criação do HelloWorldController

---
+++ src/main/factories/controllers/hello-world-factory.ts
@@ -0,0 +1,10 @@
+import { HelloWorldController } from "../../../interface-adapters/controllers/hello-world-controller";
+import { ApiHelloWorldErrorPresenter } from "../../../interface-adapters/presenters/hello-world-error-presenter";
+import { ApiHelloWorldPresenter } from "../../../interface-adapters/presenters/hello-world-presenter";
+
+export function makeHelloWorldController () {
+  return new HelloWorldController(
+    new ApiHelloWorldPresenter(),
+    new ApiHelloWorldErrorPresenter()
+  )
+}
--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,11 +1,11 @@
-import { HttpServer } from "../../frameworks-and-drivers/http-server/http-server";
-import { HelloWorldController } from "../../interface-adapters/controllers/hello-world-controller";
 import { HTTP_VERBS } from "../../interface-adapters/http/helpers";
+import { HttpServer } from "../../frameworks-and-drivers/http-server/http-server";
+import { makeHelloWorldController } from "../factories/controllers/hello-world-factory";
 
 export function setupRoutes (httpServer: HttpServer) {
   httpServer.registerController({
     method: HTTP_VERBS.GET,
-    route: '/hello-world',
-    controller: new HelloWorldController()
+    route: '/hello-world/:name',
+    controller: makeHelloWorldController()
   })
 }
